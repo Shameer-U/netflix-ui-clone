1b5e9f2763221c42ce4a5bc153c0bfcbca7ae530
Shameer-U <shameerushahul@gmail.com>
2022-11-22 00:14:16 +0530

    banner image changed

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,6 @@
 import {InfoOutlined, PlayArrow}  from '@mui/icons-material';
 import './featured.scss';
+import banner from '../../images/netflixbanner.jpg';
 
 export default function Featured({ type }) {
   return (
@@ -25,10 +26,11 @@ export default function Featured({ type }) {
           </select>
         </div>
       )}
-      <img
+      {/* <img
         src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
         alt=""
-      />
+      /> */}
+      <img src={banner} alt=""/>
       <div className="info">
         <img
           src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
